cedf16f1bdd5f74205a98245ed02101a89880494
Dave Longley <dlongley@digitalbazaar.com>
2015-06-22 11:32:47 -0400

    Setup bedrock test application.
    - Add bedrock-express as a dependency for REST API.

---
+++ bedrock/test.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+
+require('bedrock-express');
+require('bedrock-issuer');
+
+bedrock.start();
