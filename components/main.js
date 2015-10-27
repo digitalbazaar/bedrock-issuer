e36c2f22bece14e3212484f3a8c9a6befbc8f8aa
David I. Lehn <dlehn@digitalbazaar.com>
2015-10-27 13:55:37 -0400

    Component updates.
    - Fix bower file info.
    - Update main.js style.

--- components/main.js
+++ components/main.js
@@ -1,13 +1,24 @@
-define(['angular', 'underscore', 'did-io'], function(angular, _, didio) {
+/*!
+ * Issuer components module.
+ *
+ * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
+ *
+ * @author Dave Longley
+ * @author David I. Lehn
+ */
+define([
+  'angular'
+], function(angular) {
 
 'use strict';
 
-var module = angular.module('br.manualPickup', []);
+var module = angular.module(
+  'bedrock-issuer.main', Array.prototype.slice.call(arguments, 1));
 
 /* @ngInject */
 module.config(function($routeProvider) {
-
 });
 
 return module.name;
+
 });
